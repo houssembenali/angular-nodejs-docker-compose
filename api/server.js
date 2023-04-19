6da188f67e2b9c59c2963f9e921722e0d17a2d6a
Houssem Ben Ali <houssembenali90@gmail.com>
2023-04-19 04:11:05 +0200

    ANGULAR+Docker OK

--- api/server.js
+++ api/server.js
@@ -3,20 +3,20 @@ const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
 
-const users = [];
+const users = ["HOOS"];
 
 app.use(bodyParser.json());
 app.use(express.static(process.cwd()+"/my-app/dist/angular-nodejs-docker-compose/"));
 
 app.get('/api/users', (req, res) => {
   console.log('dfsdfsd')
-  res.json(users);
+  res.json("ZABOORA");
 });
 
 app.post('/api/user', (req, res) => {
   const user = req.body.user;
   users.push(user);
-  res.json("user addedd");
+  res.json("YAZEBI post");
 });
 
 app.get('/', (req,res) => {
